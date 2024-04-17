8c66c876725b62973c0af922e71c8d7679710169
Yehor Kadeniuk <91326774+newmasterSG@users.noreply.github.com>
2024-04-17 19:49:19 +0300

    Ending service department

--- src/AccountService/AccountService.API/GrpcServices/DepartmentGrpc.cs
+++ src/AccountService/AccountService.API/GrpcServices/DepartmentGrpc.cs
@@ -5,6 +5,10 @@ using Grpc.Core;
 using AccountService.API.Extensions;
 using IMediator = MediatR.IMediator;
 using System.Threading;
+using AccountService.Application.Queries.Departments;
+using static MassTransit.ValidationResultExtensions;
+using AccountService.Domain.Entity;
+using CustomHelper.Exception;
 
 namespace AccountService.API.GrpcServices
 {
@@ -56,6 +60,129 @@ namespace AccountService.API.GrpcServices
 			{
 				var department = await mediator.Send(new DeleteDepartmentCommand(Ulid.Parse(request.Id.Value)), context.CancellationToken);
 				return new Empty();
+			}
+			catch
+			{
+				throw;
+			}
+		}
+
+		public override async Task<GetDepartmentModel> GetDepartmentById(UlidDepartment request, ServerCallContext context)
+		{
+			try
+			{
+				var department = await mediator.Send(new GetDepartmentQuery(Ulid.Parse(request.Id.Value)), context.CancellationToken);
+
+				var idDepartment = GrpcMapping.ConvertToUlidGrpc(department.Id);
+
+				var result = new GetDepartmentModel
+				{
+					CreationDate = Timestamp.FromDateTime(department.CreationDate.Value),
+					Descrtiption = department.Description,
+					IdDepartment = idDepartment,
+					LastModifiedDate = Timestamp.FromDateTime(department.LastModifiedDate.Value),
+					Email = department.Email,
+					Name = department.Name,
+					PhoneNumber = department.PhoneNumber,
+				};
+
+				Parallel.ForEach(department.Users, userId =>
+				{
+					result.Users.Add(GrpcMapping.ConvertToUlidGrpc(userId));
+				});
+
+				return await Task.FromResult(result);
+			}
+			catch
+			{
+				throw;
+			}
+		}
+
+		public override async Task<GetDepartmentsGetModel> GetDepartments(GetDepartmentsModel request, ServerCallContext context)
+		{
+			try
+			{
+				var departments = await mediator.Send(new GetDepartmentsQuery(new Application.Models.Departments.GetDepartmentsDTO
+				{
+					Page = request.Page,
+					PageSize = request.PageSize,
+					SortParameters = request.SortParameters.Select(x => new Application.Models.SortParameter
+					{
+						IsDescending = x.IsDescending,
+						PropertyName = x.PropertyName,
+					}).ToList(),
+				}), context.CancellationToken);
+
+				var result = new GetDepartmentsGetModel();
+
+				var getDepartmentModels = departments.AsParallel().Select(department =>
+				{
+					var idDepartment = GrpcMapping.ConvertToUlidGrpc(department.Id);
+
+					var users = department.Users.Select(x => GrpcMapping.ConvertToUlidGrpc(x)).ToList();
+
+					return new GetDepartmentModel
+					{
+						CreationDate = Timestamp.FromDateTime(department.CreationDate.Value),
+						Descrtiption = department.Description,
+						IdDepartment = idDepartment,
+						LastModifiedDate = Timestamp.FromDateTime(department.LastModifiedDate.Value),
+						Email = department.Email,
+						Name = department.Name,
+						PhoneNumber = department.PhoneNumber,
+						Users = { users } // Add users to the Users list
+					};
+				}).ToList();
+
+				result.Departments.AddRange(getDepartmentModels);
+
+				return await Task.FromResult(result);
+			}
+			catch
+			{
+				throw;
+			}
+		}
+
+		public override async Task<GetDepartmentModel> UpdateDepartmentById(UpdateDepartmentModel request, ServerCallContext context)
+		{
+			try
+			{
+				var department = await mediator.Send(new UpdateDepartmentCommand(new Application.Models.Departments.UpdateDepartmentDTO
+				{
+					Description = request.Desctiption,
+					Email = request.Email,
+					Id = Ulid.Parse(request.Id.Value),
+					PhoneNumber = request.PhoneNumber,
+					Name = request.Name,
+					Users = request.Users.Select(x => Ulid.Parse(x.Value)).ToList(),
+				}), context.CancellationToken);
+
+				if (department == null)
+				{
+					throw new CustomException("Department is null");
+				}
+
+				var idDepartment = GrpcMapping.ConvertToUlidGrpc(department.Id);
+
+				var result = new GetDepartmentModel
+				{
+					CreationDate = Timestamp.FromDateTime(department.CreationDate.Value),
+					Descrtiption = department.Description,
+					IdDepartment = idDepartment,
+					LastModifiedDate = Timestamp.FromDateTime(department.LastModifiedDate.Value),
+					Email = department.Email,
+					Name = department.Name,
+					PhoneNumber = department.PhoneNumber,
+				};
+
+				Parallel.ForEach(request.Users, userId =>
+				{
+					result.Users.Add(userId);
+				});
+
+				return await Task.FromResult(result);
 			}
 			catch
 			{
